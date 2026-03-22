f50f7005ba520b4e7fa4c4e83a81bae36c2c56d0
Madhav Gill <221120928+manav-gill@users.noreply.github.com>
2026-03-22 20:33:34 +0530

    Remove backend files and frontend scripts, including package.json, server.js, script.js, promptbuilder.js, and style.css

--- backend/flashcardrouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const {createFlashcards} = require('./flashcardcontrllor')
-
-router.post('/', createFlashcards)
-
-module.exports = router
